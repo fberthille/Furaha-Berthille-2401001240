d99a29bbef43cf5a1ad65548d67fba26261895c4
fberthille <fberthille@gmail.com>
2025-04-13 18:33:25 +0200

    Add files via upload

---
+++ string.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   $sentence="the main body of a book";
+   echo strtolower($sentence);
+   echo strtoupper($sentence) ;
+   echo strlen($sentence);
+
+      ?> 
+</body>
+</html>
